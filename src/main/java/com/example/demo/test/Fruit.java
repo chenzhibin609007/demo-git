34318c4f34ac4dc66317860f4082d40efb96af08
83582 <835822234@qq.com>
2020-07-27 19:58:19 +0800

    xiugai

--- src/main/java/com/example/demo/test/Fruit.java
+++ src/main/java/com/example/demo/test/Fruit.java
@@ -3,6 +3,6 @@ package com.example.demo.test;
 public class Fruit {
 
     public static void main(String[] args) {
-        System.out.println("apple");
+        System.out.println("chen man yi ai chen zhi bin");
     }
 }
